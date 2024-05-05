e553b8c761b3c46c2fff214c4340f0b21a52f111
Shreyanshu <shreyanshu.15@gmail.com>
2024-05-05 19:00:49 +0530

    AstraDb

--- src/main/java/com/streaming/stream/controller/StreamingController.java
+++ src/main/java/com/streaming/stream/controller/StreamingController.java
@@ -1,11 +1,11 @@
 package com.streaming.stream.controller;
 
+import com.streaming.stream.config.AstraClient;
 import com.streaming.stream.service.StreamingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
@@ -17,6 +17,7 @@ public class StreamingController {
 
     @GetMapping(path = {"stream/{title}"}, produces = "video/mp4")
     public Mono<Resource> getController(@PathVariable String title) {
+        AstraClient dataAPIClient = new AstraClient();
         return streamingService.getVideo(title);
     }
 }
